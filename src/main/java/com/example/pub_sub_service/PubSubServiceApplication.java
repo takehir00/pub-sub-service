ab42cf56cd1df8b88ed68c29fe6f73dfc27dd1ed
okuyama <>
2024-11-21 18:29:17 +0900

    spring initializr

---
+++ src/main/java/com/example/pub_sub_service/PubSubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pub_sub_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubSubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubSubServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pub_sub_service/PubSubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pub_sub_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubSubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
