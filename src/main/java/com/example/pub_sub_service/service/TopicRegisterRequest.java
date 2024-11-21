5b1888cc3b8f469a7159f7ee0e41fdd79d8d0d43
okuyama <>
2024-11-21 18:49:45 +0900

    service topic register

---
+++ src/main/java/com/example/pub_sub_service/service/TopicRegisterRequest.java
@@ -0,0 +1,5 @@
+package com.example.pub_sub_service.service;
+
+
+public record TopicRegisterRequest(String publisherId) {
+}
---
+++ src/main/java/com/example/pub_sub_service/service/TopicRegisterService.java
@@ -0,0 +1,25 @@
+package com.example.pub_sub_service.service;
+
+import com.example.pub_sub_service.domain.PublisherId;
+import com.example.pub_sub_service.domain.Topic.Topic;
+import com.example.pub_sub_service.domain.Topic.TopicId;
+import com.example.pub_sub_service.domain.Topic.TopicRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicRegisterService {
+
+    private final TopicRepository topicRepository;
+
+    public TopicRegisterService(TopicRepository topicRepository) {
+        this.topicRepository = topicRepository;
+    }
+
+    public TopicId invoke(TopicRegisterRequest topicRegisterRequest) {
+        Topic topic = new Topic(
+                TopicId.generate(),
+                new PublisherId(topicRegisterRequest.publisherId()));
+        topicRepository.save(topic);
+        return topic.topicId();
+    }
+}
